efed0553aabd2bf326ba699a3772ecd8f1cd758f
Alexey Koptelov <koptelovav@gmail.com>
2013-04-13 12:56:33 +0400

    JsDOC

--- server/js/bullet.js
+++ server/js/bullet.js
@@ -5,14 +5,34 @@ var cls = require("./lib/class"),
     Entity = require("./entity"),
     Types = require("../../shared/js/gametypes");
 
-
+/**
+ * Фабричный класс.
+ * @type {{create: Function}}
+ */
 module.exports = BulletFactory = {
-    create: function(type){
-        return new Bullet(type);
+    /**
+     * Фабрика пуль
+     * @param {Tank} owner Владелец пули
+     * @param {Number} speed скорасть пули
+     * @param {Number} damage урон от пули
+     * @returns {Bullet} пуля
+     */
+    create: function(owner, speed, damage){
+        return new Bullet(owner, speed, damage);
     }
 };
 
+/**
+ * Класс описывющий пулю
+ * @type {Bullet}
+ */
 var Bullet = Entity.extend({
+    /**
+     * Функция конструктор. Инициализация объекта
+     * @param {Tank} owner Владелец пули
+     * @param {Number} speed скорасть пули
+     * @param {Number} damage урон от пули
+     */
     init: function(owner, speed, damage){
         this.kind = Types.Entities.BULLET;
         this.owner = owner;
@@ -22,6 +42,11 @@ var Bullet = Entity.extend({
         this.setStartingPosition();
     },
 
+    /**
+     * Получить состояние объекта
+     * @returns {Array} массив с параметрами
+     * @private
+     */
     _getBaseState: function() {
         return [
             this.kind,
@@ -33,22 +58,25 @@ var Bullet = Entity.extend({
         ];
     },
 
+    /**
+     * Устновить начальную позицю пули
+     */
     setStartingPosition: function(){
-        if(this.orientation = Types.Orientations.UP){
+        if(this.orientation === Types.Orientations.UP){
             this.x = this.owner.x;
-            this.y = this.owner.y + 0.5;
+            this.y = this.owner.y + 1;
         }
-        else if(this.orientation = Types.Orientations.DOWN){
+        else if(this.orientation === Types.Orientations.DOWN){
             this.x = this.owner.x;
-            this.y = this.owner.y - 0.5;
+            this.y = this.owner.y - 1;
         }
-        else if(this.orientation = Types.Orientations.LEFT){
-            this.x = this.owner.x - 0.5;
+        else if(this.orientation === Types.Orientations.LEFT){
+            this.x = this.owner.x - 1;
             this.y = this.owner.y;
         }
-        else if(this.orientation = Types.Orientations.RIGHT){
-            this.x = this.owner.x + 0.5;
+        else if(this.orientation === Types.Orientations.RIGHT){
+            this.x = this.owner.x + 1;
             this.y = this.owner.y;
-        }s
+        }
     }
 });
