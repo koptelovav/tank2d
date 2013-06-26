377415056a539cbe2fbada070ef9b7546f241c5a
alexey.koptelov <alexey.koptelov@nevosoft.ru>
2013-06-26 12:26:12 +0400

    refactoring

--- client/js/game.js
+++ client/js/game.js
@@ -1,11 +1,11 @@
-define(['../../shared/js/gamebase','../../shared/js/bullet','spritemanager','scene', '../../shared/js/map', '../../shared/js/tilefactory', '../../shared/js/player', 'connection','../../shared/js/gametypes'],
-    function (GameBase,Bullet,SpriteManager,Scene, Map, TileFactory, Player, Connection) {
+define(['../../shared/js/gamebase', '../../shared/js/bullet', 'spritemanager', 'scene', '../../shared/js/map', '../../shared/js/tilefactory', '../../shared/js/player', 'connection', '../../shared/js/gametypes'],
+    function (GameBase, Bullet, SpriteManager, Scene, Map, TileFactory, Player, Connection) {
 
         var Game = GameBase.extend({
             init: function (app) {
                 this.app = app;
                 this.spriteManager = new SpriteManager();
-                this.connection = new Connection('127.0.0.1','8000');
+                this.connection = new Connection('127.0.0.1', '8000');
                 this.ready = false;
                 this.started = false;
                 this.connected = false;
@@ -24,7 +24,7 @@ define(['../../shared/js/gamebase','../../shared/js/bullet','spritemanager','sce
             },
 
             setup: function (entities, background, foreground) {
-                this.scene = new Scene(768,768);
+                this.scene = new Scene(768, 768);
                 this.scene.newLayer('entities', entities);
                 this.scene.newLayer('background', background);
                 this.scene.newLayer('foreground', foreground);
@@ -39,14 +39,14 @@ define(['../../shared/js/gamebase','../../shared/js/bullet','spritemanager','sce
                 this.spriteManager.addResource(this.spriteNames).load();
 
                 var waitGameLoadData = setInterval(function () {
-                        var waitStartGame = setInterval(function () {
-                            if (self.map && self.map.isLoaded && self.started) {
-                                self.start();
-                                self.connection.send(Types.Messages.LOADMAP);
-                                clearInterval(waitStartGame);
-                            }
-                        }, 1000);
-                        clearInterval(waitGameLoadData);
+                    var waitStartGame = setInterval(function () {
+                        if (self.map && self.map.isLoaded && self.started) {
+                            self.start();
+                            self.connection.send(Types.Messages.LOADMAP);
+                            clearInterval(waitStartGame);
+                        }
+                    }, 1000);
+                    clearInterval(waitGameLoadData);
                 }, 100);
             },
 
@@ -91,17 +91,17 @@ define(['../../shared/js/gamebase','../../shared/js/bullet','spritemanager','sce
                 }
             },
 
-            addToScene: function(entity){
+            addToScene: function (entity) {
                 var element = this.scene.createElement(entity);
                 element.setSprite(this.spriteManager.getSprite(entity.kind));
                 element.setAnimation('idle', 800);
                 element.isAnimated = Types.getIsAnimateAsKind(entity.kind);
 
-                this.scene.addToLayer(element,Types.getLayerAsKind(entity.kind));
+                this.scene.addToLayer(element, Types.getLayerAsKind(entity.kind));
             },
 
-            removeFromScene: function(entity){
-                this.scene.removeFromLayer(entity,Types.getLayerAsKind(entity.kind));
+            removeFromScene: function (entity) {
+                this.scene.removeFromLayer(entity, Types.getLayerAsKind(entity.kind));
             },
 
 
@@ -118,23 +118,23 @@ define(['../../shared/js/gamebase','../../shared/js/bullet','spritemanager','sce
                 }
             },
 
-            moveEntities: function(){
-                _.each(this.movableEntities, function(entity){
-                    if(entity.isMovable){
-                        if(entity instanceof Player){
-                           if(this.isValidPlayerMove(entity)){
+            moveEntities: function () {
+                _.each(this.movableEntities, function (entity) {
+                    if (entity.isMovable) {
+                        if (entity instanceof Player) {
+                            if (this.isValidPlayerMove(entity)) {
                                 entity.move();
                             }
-                        }else if(entity instanceof Bullet){
-                            if(!this.map.isOutOfBounds.apply(this.map, entity.move(1, true))){
+                        } else if (entity instanceof Bullet) {
+                            if (!this.map.isOutOfBounds.apply(this.map, entity.move(1, true))) {
                                 var hit = this.map.isBulletColliding.call(this.map, entity);
-                                if(_.isObject(hit) && !_.isEmpty(hit)){
-                                    _.each(hit, function(item){
-                                       if(item.strength <= entity.damage)
+                                if (_.isObject(hit) && !_.isEmpty(hit)) {
+                                    _.each(hit, function (item) {
+                                        if (item.strength <= entity.damage)
                                             this.removeEntity(item);
                                     }, this);
                                 }
-                                else{
+                                else {
                                     entity.move();
                                     return;
                                 }
@@ -147,7 +147,7 @@ define(['../../shared/js/gamebase','../../shared/js/bullet','spritemanager','sce
                 }, this);
             },
 
-            removeEntity: function(entity){
+            removeEntity: function (entity) {
                 this.removeFromScene(entity);
                 this.removeFromEntityGrid(entity, entity.gridX, entity.gridY);
                 delete this.entities[entity.id];
@@ -157,17 +157,17 @@ define(['../../shared/js/gamebase','../../shared/js/bullet','spritemanager','sce
             connect: function () {
                 this.connection.connect();
 
-                this.connection.on('connect',function () {
+                this.connection.on('connect', function () {
                     this.connection.send(Types.Messages.HELLO);
                 }, this);
 
-                this.connection.on('welcome',function (playerData) {
+                this.connection.on('welcome', function (playerData) {
                     this.connected = true;
                     this.player = this.addPlayer(playerData);
                     this.emit('playerWelcome');
                 }, this);
 
-                this.connection.on('gameData',function (id, population, teamCount, minPlayers, maxPlayers, players) {
+                this.connection.on('gameData', function (id, population, teamCount, minPlayers, maxPlayers, players) {
                     this.id = id;
                     this.setPopulation(population);
                     this.teamCount = teamCount;
@@ -181,37 +181,37 @@ define(['../../shared/js/gamebase','../../shared/js/bullet','spritemanager','sce
                     this.emit('load');
                 }, this);
 
-                this.connection.on('gameStart',function () {
+                this.connection.on('gameStart', function () {
                     this.started = true;
                     this.emit('start');
                 }, this);
 
-                this.connection.on('sendMap',function (data) {
+                this.connection.on('sendMap', function (data) {
                     this.loadMap(data);
                 }, this);
 
-                this.connection.on('joinGame',function (playerData) {
+                this.connection.on('joinGame', function (playerData) {
                     this.addPlayer(playerData);
                     this.incrementPopulation();
                 }, this);
 
-                this.connection.on('leftGame',function (playerId) {
+                this.connection.on('leftGame', function (playerId) {
                     this.decrementPopulation();
                     delete this.entities[playerId];
 
-                    this.emit('playerLeft',playerId);
+                    this.emit('playerLeft', playerId);
                 }, this);
 
-                this.connection.on('iReady',function (playerId) {
-                    this.emit('playerReady',playerId);
+                this.connection.on('iReady', function (playerId) {
+                    this.emit('playerReady', playerId);
                 }, this);
 
-                this.connection.on('gamePlay',function () {
+                this.connection.on('gamePlay', function () {
 //                    this.connection.send(Types.Messages.SENDMAP);
                     this.emit('play');
                 }, this);
 
-                this.connection.on('spawn',function (id, x, y, orientation) {
+                this.connection.on('spawn', function (id, x, y, orientation) {
                     var player;
                     if (this.entityIdExists(id)) {
                         player = this.getEntityById(id);
@@ -221,34 +221,34 @@ define(['../../shared/js/gamebase','../../shared/js/bullet','spritemanager','sce
 
                         this.addToScene(player);
 
-                        player.on('move',function(){
+                        player.on('move', function () {
                             this.addToEntityGrid(player);
                         }, this);
                     }
                 }, this);
 
-                this.connection.on('chat',function(id, message){
-                    this.emit('chatMessage',id, message);
+                this.connection.on('chat', function (id, message) {
+                    this.emit('chatMessage', id, message);
                 }, this);
 
-                this.connection.on('move',function (id, orientation) {
+                this.connection.on('move', function (id, orientation) {
                     this.playerMove(id, orientation);
                 }, this);
 
-                this.connection.on('endMove',function (id) {
+                this.connection.on('endMove', function (id) {
                     this.playerStopMove(id);
                 }, this);
 
-                this.connection.on('syncPos',function (id ,x, y, gridX, gridY) {
+                this.connection.on('syncPos', function (id, x, y, gridX, gridY) {
                     this.entities[id].syncPososition(x, y, gridX, gridY);
                 }, this);
             },
 
-            addPlayer: function(d){
-                var player = new Player(d[0],d[1],d[2],d[5],d[6]);
-                player.setPosition(d[3],d[4]);
+            addPlayer: function (d) {
+                var player = new Player(d[0], d[1], d[2], d[5], d[6]);
+                player.setPosition(d[3], d[4]);
                 this.addMovableEntity(player);
-                this.emit('playerJoin',player);
+                this.emit('playerJoin', player);
                 return player;
             },
 
@@ -260,22 +260,22 @@ define(['../../shared/js/gamebase','../../shared/js/bullet','spritemanager','sce
                 this.connection.send(Types.Messages.CHAT, message);
             },
 
-            playerFire: function(id){
-                if(this.player.canFire()){
-                this.player.toggleFire();
-                var bullet = new Bullet(Date.now(), 'easy', 'bullet', this.player, 8);
-                this.addMovableEntity(bullet);
-                this.addToScene(bullet);
-                bullet.toggleMovable();
+            playerFire: function (id) {
+                if (this.player.canFire()) {
+                    this.player.toggleFire();
+                    var bullet = new Bullet(Date.now(), 'easy', 'bullet', this.player, 8);
+                    this.addMovableEntity(bullet);
+                    this.addToScene(bullet);
+                    bullet.toggleMovable();
                 }
             },
 
-            playerStopMove: function(id){
+            playerStopMove: function (id) {
                 var player = id ? this.entities[id] : this.player;
 
-                if(player.isMovable){
+                if (player.isMovable) {
                     player.toggleMovable();
-                    if(!id){
+                    if (!id) {
 //                        this.listener.sendEndMove();
                     }
                 }
@@ -297,9 +297,9 @@ define(['../../shared/js/gamebase','../../shared/js/bullet','spritemanager','sce
                 var player = id ? this.getEntityById(id) : this.player;
                 player.setOrientation(orientation);
 
-                if(!player.isMovable){
+                if (!player.isMovable) {
                     player.toggleMovable();
-                    if(!id){
+                    if (!id) {
                         this.connection.send(Types.Messages.MOVE, orientation);
                     }
                 }
@@ -307,14 +307,24 @@ define(['../../shared/js/gamebase','../../shared/js/bullet','spritemanager','sce
 
             isValidPlayerMove: function (player) {
                 if (this.map && player) {
-                    var result = true,
-                        chunk = player.getChunk(true);
+                    var x,
+                        y,
+                        chunk = player.getChunk(true),
+                        result = _.any(chunk, function (pos) {
+                            x = pos[0];
+                            y = pos[1];
 
-                    _.each(chunk, function(pos){
-                        result = result && !this.map.isTankColliding.call(this.map,pos[0],pos[1],player.id);
-                    },this);
+                            if (this.map.isOutOfBounds(x, y)) {
+                                return true;
+                            }
+                            for (var id in this.entityGrid[x][y]) {
+                                if (player.id != id && Types.getCollidingArray(this.entityGrid[x][y][id].kind).indexOf('tank') >= 0)
+                                    return true;
+                            }
+                            return false;
+                        }, this);
                 }
-                return result;
+                return !result;
             }
         });
 
