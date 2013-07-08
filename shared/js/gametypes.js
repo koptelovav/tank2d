af40966b9eae2cc927a73ea032859dd9143a8374
Alexey Koptelov <koptelovav@gmail.com>
2013-07-08 22:22:44 +0400

    refactoring

--- shared/js/gametypes.js
+++ shared/js/gametypes.js
@@ -1,7 +1,7 @@
 Types = {
-    Environment:{
-      CLIENT: 1,
-      SERVER: 2
+    Environment: {
+        CLIENT: 1,
+        SERVER: 2
     },
     Layers: {
         ENTITIES: 1,
@@ -27,7 +27,7 @@ Types = {
         IREADY: 7,
         GAMEDATA: 8,
         GAMEFULL: 9,
-        LOADMAP : 10,
+        LOADMAP: 10,
         GAMEPLAY: 11,
         SPAWN: 12,
         CHAT: 13,
@@ -69,7 +69,7 @@ Types = {
         MOVABLE: 1,
         PLAYER: 2,
         BULLET: 3,
-        TILE : 4
+        TILE: 4
     },
 
     Orientations: {
@@ -98,86 +98,94 @@ var messages = {
     'connect': [Types.Messages.CONNECT],
     'sendMap': [Types.Messages.SENDMAP],
 
-    getType: function(message) {
+    getType: function (message) {
         return messages[Types.getMessageName(message)][1];
     }
 };
 
-Types.getMessageName = function(code) {
-    for(var name in messages) {
-        if(messages[name][0] === code) {
+Types.getMessageName = function (code) {
+    for (var name in messages) {
+        if (messages[name][0] === code) {
             return name;
         }
     }
 };
 
-Types.getMessageCode = function(name) {
+Types.getMessageCode = function (name) {
     return messages[name][0];
 };
 
-var kindConfig = {
-    base: {
-        layer: Types.Layers.BACKGROUND,
-        animated: false,
-        strength: 0,
-        width: 32,
-        height: 32,
-        colliding: [Types.Entities.TANK,Types.Entities.BULLET]
-    },
-    ice: {
-        layer: Types.Layers.BACKGROUND,
-        animated: false,
-        strength: 0,
-        width: 16,
-        height: 16,
-        colliding: []
-    },
-    wall: {
-        layer: Types.Layers.BACKGROUND,
-        animated: false,
-        strength: 30,
-        width: 16,
-        height: 16,
-        colliding: [Types.Entities.TANK,Types.Entities.BULLET]
-    },
-    armoredwall: {
-        layer: Types.Layers.BACKGROUND,
-        animated: false,
-        strength: 60,
-        width: 16,
-        height: 16,
-        colliding: [Types.Entities.TANK,Types.Entities.BULLET]
-    },
-    trees: {
-        layer: Types.Layers.FOREGROUND,
-        animated: false,
-        strength: 0,
-        width: 16,
-        height: 16,
-        colliding: []
-    },
-    water: {
-        layer: Types.Layers.BACKGROUND,
-        animated: true,
-        strength: 0,
-        width: 16,
-        height: 16,
-        colliding: [Types.Entities.TANK]
-    },
-    tank: {
-        layer: Types.Layers.ENTITIES,
-        animated: true,
-        strength: 1,
-        colliding: ['tank','bullet','wall','armoredwall','water']
+var kindConfig = {};
 
-    },
-    bullet: {
-        layer: Types.Layers.ENTITIES,
-        animated: false,
-        strength: 1,
-        colliding: ['tank','bullet','wall','armoredwall']
-    }
+kindConfig[Types.Entities.BASE] = {
+    layer: Types.Layers.BACKGROUND,
+    animated: false,
+    strength: 0,
+    width: 32,
+    height: 32,
+    colliding: [Types.Entities.TANK, Types.Entities.BULLET]
 };
+
+kindConfig[Types.Entities.ICE] = {
+    layer: Types.Layers.BACKGROUND,
+    animated: false,
+    strength: 0,
+    width: 16,
+    height: 16,
+    colliding: []
+};
+
+kindConfig[Types.Entities.WALL] = {
+    layer: Types.Layers.BACKGROUND,
+    animated: false,
+    strength: 30,
+    width: 16,
+    height: 16,
+    colliding: [Types.Entities.TANK, Types.Entities.BULLET]
+};
+
+kindConfig[Types.Entities.ARMOREDWALL] = {
+    layer: Types.Layers.BACKGROUND,
+    animated: false,
+    strength: 60,
+    width: 16,
+    height: 16,
+    colliding: [Types.Entities.TANK, Types.Entities.BULLET]
+};
+
+kindConfig[Types.Entities.TREES] = {
+    layer: Types.Layers.FOREGROUND,
+    animated: false,
+    strength: 0,
+    width: 16,
+    height: 16,
+    colliding: []
+};
+
+kindConfig[Types.Entities.WATER] = {
+    layer: Types.Layers.BACKGROUND,
+    animated: true,
+    strength: 0,
+    width: 16,
+    height: 16,
+    colliding: [Types.Entities.TANK]
+};
+
+kindConfig[Types.Entities.TANK] = {
+    layer: Types.Layers.ENTITIES,
+    animated: true,
+    strength: 1,
+    colliding: ['tank', 'bullet', 'wall', 'armoredwall', 'water']
+
+};
+
+kindConfig[Types.Entities.BULLET] = {
+    layer: Types.Layers.ENTITIES,
+    animated: false,
+    strength: 1,
+    colliding: ['tank', 'bullet', 'wall', 'armoredwall']
+};
+
 
 var kinds = {
     tank: [Types.Entities.TANK, "player"],
@@ -201,103 +209,111 @@ var kinds = {
     water: [Types.Entities.WATER, "tile"],
     base: [Types.Entities.BASE, "tile"],
 
-    getType: function(kind) {
+    getType: function (kind) {
         return kinds[Types.getKindAsString(kind)][1];
     },
-    getKindString: function(kind) {
-        for(var name in kinds){
-            if(kinds[name][0] === kind)
+    getKindString: function (kind) {
+        for (var name in kinds) {
+            if (kinds[name][0] === kind)
                 return name;
         }
     }
 };
 
-Types.getKindLayer = function(kind) {
-    return kindConfig[kinds.getKindString(kind)]['layer'];
+Types.getKindLayer = function (kind) {
+    return kindConfig[kind]['layer'];
 };
-Types.getIsAnimateAsKind = function(kind) {
-    return kindConfig[kinds.getKindString(kind)]['animated'];
-};
-
-Types.getCollidingArray = function(kind) {
-    return kindConfig[kinds.getKindString(kind)]['colliding'];
+Types.getIsAnimateAsKind = function (kind) {
+    return kindConfig[kind]['animated'];
 };
 
-Types.getKindConfig = function(kind, param) {
-   // console.log(kinds.getKindString(kind),kind);
-    if(param) return kindConfig[kinds.getKindString(kind)][param];
-    else return kindConfig[kinds.getKindString(kind)];
+Types.getCollidingArray = function (kind) {
+    return kindConfig[kind]['colliding'];
+};
+
+Types.getKindConfig = function (kind, param) {
+    // console.log(kinds.getKindString(kind),kind);
+    if (param) return kindConfig[kind][param];
+    else return kindConfig[kind];
 
 };
 
-Types.getKindString = function(kind) {
+Types.getKindString = function (kind) {
     return kinds.getKindString(kind);
 };
 
-Types.isPlayer = function(kind) {
+Types.isPlayer = function (kind) {
     return kinds.getType(kind) === "player";
 };
 
-Types.isMapElement = function(kind) {
+Types.isMapElement = function (kind) {
     return kinds.getType(kind) === "mapelement";
 };
 
-Types.isObject = function(kind) {
+Types.isObject = function (kind) {
     return kinds.getType(kind) === "object";
 };
 
-Types.getKindFromString = function(kind) {
-    if(kind in kinds) {
+Types.getKindFromString = function (kind) {
+    if (kind in kinds) {
         return kinds[kind][0];
     }
 };
 
-Types.getKindAsString = function(kind) {
-    for(var k in kinds) {
-        if(kinds[k][0] === kind) {
+Types.getKindAsString = function (kind) {
+    for (var k in kinds) {
+        if (kinds[k][0] === kind) {
             return k;
         }
     }
 };
 
-Types.forEachKind = function(callback) {
-    for(var k in kinds) {
+Types.forEachKind = function (callback) {
+    for (var k in kinds) {
         callback(kinds[k][0], k);
     }
 };
 
 
-Types.getOrientationAsString = function(orientation) {
-    switch(orientation) {
-        case Types.Orientations.LEFT: return "left"; break;
-        case Types.Orientations.RIGHT: return "right"; break;
-        case Types.Orientations.UP: return "up"; break;
-        case Types.Orientations.DOWN: return "down"; break;
+Types.getOrientationAsString = function (orientation) {
+    switch (orientation) {
+        case Types.Orientations.LEFT:
+            return "left";
+            break;
+        case Types.Orientations.RIGHT:
+            return "right";
+            break;
+        case Types.Orientations.UP:
+            return "up";
+            break;
+        case Types.Orientations.DOWN:
+            return "down";
+            break;
     }
 };
 
-Types.getRandomItemKind = function(item) {
+Types.getRandomItemKind = function (item) {
     var all = _.union(this.rankedWeapons, this.rankedArmors),
         forbidden = [Types.Entities.SWORD1, Types.Entities.CLOTHARMOR],
         itemKinds = _.difference(all, forbidden),
         i = Math.floor(Math.random() * _.size(itemKinds));
-    
+
     return itemKinds[i];
 };
 
-Types.getMessageTypeAsString = function(type) {
+Types.getMessageTypeAsString = function (type) {
     var typeName;
-    _.each(Types.Messages, function(value, name) {
-        if(value === type) {
+    _.each(Types.Messages, function (value, name) {
+        if (value === type) {
             typeName = name;
         }
     });
-    if(!typeName) {
+    if (!typeName) {
         typeName = "UNKNOWN";
     }
     return typeName;
 };
 
-if(!(typeof exports === 'undefined')) {
+if (!(typeof exports === 'undefined')) {
     module.exports = Types;
 }
